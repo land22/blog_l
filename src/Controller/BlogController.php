9c71d32e188eb4ccb41e1ca2c409f729266f331a
landry wabo <landrywabo8@gmail.com>
2019-10-17 09:43:28 +0100

    fin du tuto de creation de formulaire

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -3,9 +3,15 @@
 namespace App\Controller;
 
 use App\Entity\Article;
+use App\Form\ArticleType;
 use App\Repository\ArticleRepository;
+use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 class BlogController extends AbstractController
 {
@@ -30,6 +36,36 @@ class BlogController extends AbstractController
     }
 
     /**
+     * @Route("blog/new", name="blog_create")
+     * @Route("/blog/{id}/edit",name="blog_edit")
+     */
+    public function create_add( Article $article = null, Request $request, ObjectManager $manager){
+        if (!$article){
+            $article = new Article();
+        }
+
+        /*$form = $this->createFormBuilder($article)
+            ->add('title')
+            ->add('content')
+            ->add('image')
+            ->getForm();*/
+        $form = $this->createForm(ArticleType::class, $article);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()){
+            if (!$article->getId()){
+                $article->setCreatedAt(new \DateTime());
+            }
+            $manager->persist($article);
+            $manager->flush();
+            return $this->redirectToRoute('blog_show',['id'=>$article->getId()]);
+        }
+
+        return $this->render('blog/create.html.twig',[
+            'formArticle' => $form->createView(), 'editMode'=> $article->getId() !== null
+        ]);
+    }
+
+    /**
      * @Route("/blog/{id}", name="blog_show")
      */
 
@@ -38,4 +74,6 @@ class BlogController extends AbstractController
             'article' =>$article
         ]);
     }
+
+
 }
